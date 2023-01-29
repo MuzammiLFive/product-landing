a0d125432457f67bbeef1ca663ddef073a95eccc
Sai Aditya <95907651+Aditya-Ak44@users.noreply.github.com>
2023-01-29 21:17:56 +0000

    changes for more readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
+import Header from './components/header/header';
 
 function App() {
   return (
