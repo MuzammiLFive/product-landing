90a72ff88f150bc7d4fa2965c7d7c8f205cbc77c
Muzammil <muzammilfive@gmail.com>
2023-02-03 01:00:15 +0000

    Added routing and empty content components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
+import {Footer} from "./components/footer/footer";
+
+import {
+    Routes,
+    Route
+} from "react-router-dom";
+import {Home} from "./components/contents/home/home";
+import {Menu} from "./components/contents/menu/menu";
+import {Feedback} from "./components/contents/feedback/feedback";
+import {ContactUs} from "./components/contents/contact-us/contact-us";
+
+export enum Pages {
+    Home = "/",
+    Menu = "/menu",
+    Feedback = "/feedback",
+    ContactUs = "/contact-us"
+}
 
 function App() {
-  return (
-    <div >
-      <Header/>
-    </div>
-  );
+    return (
+        <>
+            <div className="content">
+                <Header/>
+                <Routes>
+                    <Route path={Pages.Home} element={<Home/>}></Route>
+                    <Route path={Pages.Menu} element={<Menu/>}/>
+                    <Route path={Pages.ContactUs} element={<ContactUs/>}/>
+                    <Route path={Pages.Feedback} element={<Feedback/>}/>
+                </Routes>
+                <Footer/>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/contents/contact-us/contact-us.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class ContactUs extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                Contact Us
+            </div>
+        )
+    }
+}
